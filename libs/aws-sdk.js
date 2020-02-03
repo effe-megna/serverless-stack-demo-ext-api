fb716447a4ae85970c85167fefc1a38babae0829
effe-m <francesco.megna97@gmail.com>
2020-02-04 00:16:29 +0100

    semicolon

--- libs/aws-sdk.js
+++ libs/aws-sdk.js
@@ -1,7 +1,7 @@
 import aws from "aws-sdk";
 import xray from "aws-xray-sdk";
 
-console.log("DAFUCK")
+console.log("DAFUCK");
 
 // Do not enable tracing for 'invoke local'
 const awsWrapped = process.env.IS_LOCAL ? aws : xray.captureAWS(aws);
