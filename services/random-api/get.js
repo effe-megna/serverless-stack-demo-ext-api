bded4eed5bd9b89de602d9d7defba367d5cbb7ad
effe-m <francesco.megna97@gmail.com>
2020-02-04 00:35:08 +0100

    create random-api

---
+++ services/random-api/get.js
@@ -0,0 +1,8 @@
+import * as dynamoDbLib from "../../libs/dynamodb-lib";
+import { success, failure } from "../../libs/response-lib";
+
+export async function main(event, context) {
+  return success({
+    hello: "world!"
+  })
+}
