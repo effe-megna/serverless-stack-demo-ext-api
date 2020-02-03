5a527ac3df136e22caa9094f0280eb673729ffb6
effe-m <francesco.megna97@gmail.com>
2020-02-04 00:11:25 +0100

    dafuck commit

--- libs/aws-sdk.js
+++ libs/aws-sdk.js
@@ -1,6 +1,8 @@
 import aws from "aws-sdk";
 import xray from "aws-xray-sdk";
 
+console.log("DAFUCK")
+
 // Do not enable tracing for 'invoke local'
 const awsWrapped = process.env.IS_LOCAL ? aws : xray.captureAWS(aws);
 
